f67c952f14eb90ef51316b7dd31e0761e50fd1b5
Paul Clements <paul.clements@us.sios.com>
2018-06-29 12:08:01 -0400

    updates to cluster model

--- python/SignaliQ/model/Cluster.py
+++ python/SignaliQ/model/Cluster.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2017-2018 SIOS Technology Corp. All rights reserved.
 ##############################################################################
 import logging
+from enum import Enum
 __log__ = logging.getLogger(__name__)
 
 
@@ -11,11 +12,24 @@ class Cluster(object):
     """
     Model for clusters. Typically, the UUID is provided by the environment.
 
+    :param str name: Name for the Cluster.
     :param str uuid: Unique identifier for the Cluster.
+    :param str state: State of the Cluster.
+    :param ClusterType type: Type of Cluster.
     """
-    def __init__(self, uuid = ""):
-        if not uuid:
-            __log__.error("Must provide the uuid!")
+    def __init__(self, name, uuid, type):
+        if not uuid or not name or not state:
+            __log__.error("Must provide the name and uuid!")
             return
 
+        if not isinstance(type, ClusterType):
+            raise TypeError('cluster type must be an instance of ClusterType')
+
+        self.name = name
         self.uuid = uuid
+
+# Enum for cluster type.
+class ClusterType(Enum):
+    vSphere = "vSphere"
+    WSFC = "WSFC"
+    LifeKeeper = "LifeKeeper"
